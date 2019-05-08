9fdddbb4f56979e06880cd5ec5319ed63c4b985b
yangtao <438936731@qq.com>
2019-05-08 15:06:42 +0800

    viewsggg

--- api/views.py
+++ api/views.py
@@ -19,22 +19,33 @@ text-268:
         name = request.POST.get("text-768")
         phone = request.POST.get("text-939")
         loan_money = request.POST.get("text-268")
-        try:
-            if len(name)>=5 or len(name)<1:
-                raise Exception("name:%s error"%name)
+        if name and phone and loan_money:
+            name = name.strip()
+            phone = phone.strip()
+            loan_money = loan_money.strip()
+            try:
+                # 验证姓名
+                if len(name)>=5:
+                    raise Exception("name:%s error"%name)
 
-            m = re.findall(r"'13\d{9}|14\d{9}|15\d{9}|16\d{9}|17\d{9}|18\d{9}|19\d{9}'", phone)
-            if not m:
-                raise Exception("phone:%s error"%phone)
+                # 验证电话
+                m = re.match(r"13\d{9}|14\d{9}|15\d{9}|16\d{9}|17\d{9}|18\d{9}|19\d{9}", phone)
+                if not m:
+                    raise Exception("phone:%s error"%phone)
+
+                # 验证借款金额
+                key_words = ["元","万","千","百","十","亿","RMB","人民币"]
+                for i in key_words:
+                    loan_money = loan_money.replace(i,"")
 
 
-            APILoan.objects.create(
-                name=name,
-                phone = phone,
-                loan_money=float(loan_money)
-            )
-        except:
-            logger.exception("存储出错")
+                APILoan.objects.create(
+                    name=name,
+                    phone = phone,
+                    loan_money=float(loan_money)
+                )
+            except:
+                logger.exception("存储出错")
 
 
-    return HttpResponseRedirect("http://219.153.96.167:8000")
+    return HttpResponseRedirect("/")
